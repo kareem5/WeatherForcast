ba9a9498a84be9f3919d76d8f89cf8e02f853284
kareem5 <k_m52@hotmail.com>
2022-03-13 18:40:31 +0200

    1. Created Network Layer to handle API calls.

---
+++ WeatherForecast/Helpers/Network/APIClient.swift
@@ -0,0 +1,43 @@
+//
+//  APIClient.swift
+//  WeatherForecast
+//
+//  Created by Kareem Ahmed on 13/03/2022.
+//
+
+import Combine
+import Foundation
+
+protocol APIClient {
+    func fetch<T>(endpoint: Endpoint, type: T.Type, params: [String: Any]) -> AnyPublisher<T, Error> where T: Decodable
+}
+
+extension APIClient {
+    func fetch<T>(endpoint: Endpoint, type: T.Type, params: [String: Any]) -> AnyPublisher<T, Error> where T: Decodable {
+        var urlRequest = URLRequest(url: endpoint.baseURL.appendingPathComponent(endpoint.path))
+        urlRequest.httpMethod = endpoint.method.rawValue
+        endpoint.headers?.forEach {
+            urlRequest.addValue($0.value, forHTTPHeaderField: $0.key)
+        }
+
+        let body = try? JSONSerialization.data(withJSONObject: params, options: [])
+        print("body: \(String(describing: body))")
+        urlRequest.httpBody = body
+        
+        
+        print("urlRequest: \(urlRequest.description)")
+        return URLSession.shared.dataTaskPublisher(for: urlRequest)
+//                    .tryMap({ element -> Data in
+//                        guard let httpResponse = element.response as? HTTPURLResponse,
+//                            httpResponse.statusCode == 200 else {
+//                                throw URLError(.badServerResponse)
+//                            }
+//                        let str = String(decoding: element.data, as: UTF8.self)
+//                        print("response: \(str)")
+//                        return element.data
+//                        })
+            .map(\.data)
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+}
